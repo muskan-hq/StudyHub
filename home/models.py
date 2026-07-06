0029c530a6a95dd37c66d80a269b69375b3f387f
Muskan Kumari <muskankumarijnb2006@gmail.com>
2026-07-06 13:18:46 +0530

    Add Task model and Django admin

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    time = models.CharField(max_length=50)
+    notes = models.TextField()
+    score = models.IntegerField()
+    submission = models.BooleanField()
+
+    def __str__(self):
+        return self.task_name
