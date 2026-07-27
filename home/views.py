247df0c152ce5c55f03a5e498082e473c8870fdd
Muskan Kumari <muskankumarijnb2006@gmail.com>
2026-07-27 15:08:57 +0530

    Build HTML form using POST request for task creation

--- home/views.py
+++ home/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render, redirect
 from .models import Task
 
 def home(request):
+
+    if request.method == "POST":
+        task_name = request.POST["task_name"]
+
+        if task_name:
+
+            task = Task(
+                task_name=request.POST["task_name"],
+                time="1 hour",
+                notes="Temporary Notes",
+                score=0,
+                submission=False
+            )
+
+            task.save()
+
     tasks = Task.objects.all()
+
     context = {
-        "name": "Muskan",
+         "name": "Muskan",
         "project": "StudyHub",
         "year": "Final year",
         "placement": False,
